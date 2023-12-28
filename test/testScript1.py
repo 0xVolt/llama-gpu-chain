691cf31f9cf2071d2c4f99edd4b15b977e1190f6
Desh Iyer <desh.iyer@gmail.com>
2023-12-28 18:33:42 +0400

    Rename : Notebook to run gpu inferences

--- test/testScript1.py
+++
@@ -1,7 +0,0 @@
-def checkGPU(tensorflow):
-    if tensorflow == True:
-        import tensorflow as tf
-        print("Number of GPUs available with tensorflow:", len(tf.config.list_physical_devices('GPU')))
-    else:
-        import torch
-        print('Checking if the GPU is available with PyTorch:', torch.cuda.is_available())
